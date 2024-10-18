3890d95afd0a8597236c5dbbc7032d127a58864c
Pablo Querales <pabloquerales20@gmail.com>
2024-10-19 01:04:56 +0200

    se crearon las funciones básicas de suma, resta y multiplicación

--- scrip.js
+++ scrip.js
@@ -3,5 +3,26 @@ function sum(num1,num2){
   let result = num1 + num2
   return result
 }
-let output = sum(1,2)
+let output = sum(1,200)
 console.log(output)
+// Función encargada de la RESTA
+function sust(num1,num2){
+  let result = num1 - num2
+  return result
+}
+let output1 = sust(1,200)
+console.log(output1)
+// Función encargada de la MULTIPLICACIÓN
+function mult(num1,num2){
+  let result = num1 * num2
+  return result
+}
+let output2 = mult(1,200)
+console.log(output2)
+// Función encargada de la DIVISIÓN
+function divi(num1,num2){
+  let result = num1 / num2
+  return result
+}
+let output3 = divi(1,200)
+console.log(output3)
