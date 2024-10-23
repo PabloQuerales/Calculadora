af924cc2c6221e047bf1276bb54e844ee68d4e71
Pablo Querales <pabloquerales20@gmail.com>
2024-10-23 13:54:17 +0200

    se hicieron pruebas con una nueva función para concatenar texto

--- scrip.js
+++ scrip.js
@@ -1,10 +1,18 @@
-let keyboardId = document.querySelector("#keyboard-id")
+let keyboardId = document.querySelector(".numbers")
 let display = document.querySelector("p")
 
 let n1 = []
+let n2 = []
+let result = []
+
 keyboardId.addEventListener("click",(e)=>{
   n1.push(e.target.textContent)
   let number = n1
-  console.log(number)
   display.textContent = number
+  concatenar()
 })
+function concatenar () {
+  for (let i = 0; i < n1.length; i++) {
+    console.log("HolaMundo")
+  }
+}
