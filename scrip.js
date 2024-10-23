3d5297f1216abe9778a4e2861c40933a01507bde
Pablo Querales <pabloquerales20@gmail.com>
2024-10-23 13:10:17 +0200

    se borraron las antiguas funciones para reducir las lineas de código

--- scrip.js
+++ scrip.js
@@ -1,42 +1,3 @@
-// Función encargada de la SUMA
-function sum(impr,impr){
-  let result = impr + impr
-  return result
-}
-let output = sum(1,200)
-console.log(output)
-// Función encargada de la RESTA
-function sust(num1,num2){
-  let result = num1 - num2
-  return result
-}
-let output1 = sust(1,200)
-console.log(output1)
-// Función encargada de la MULTIPLICACIÓN
-function mult(num1,num2){
-  let result = num1 * num2
-  return result
-}
-let output2 = mult(1,200)
-console.log(output2)
-// Función encargada de la DIVISIÓN
-function divi(num1,num2){
-  let result = num1 / num2
-  return result
-}
-let output3 = divi(1,200)
-console.log(output3)
-
-
-
-
-
-
-
-
-
-
-
 let num2 = document.getElementById("keyboard-id")
 
 let n1 = []
@@ -46,10 +7,6 @@ num2.addEventListener("click",(e)=>{
 })
 
 num2.addEventListener("click",(e)=>{
-console.log(n1)
+  let number = ""
+  console.log(number)
 })
-console.log()
-for (let i = 0; i < n1.length; i++) {
-  const element = array[i];
-  return element
-}
