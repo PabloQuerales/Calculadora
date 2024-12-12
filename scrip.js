083ff82d6cb9b98d02f06d8cae27129605b5c001
Pablo Querales <pabloquerales20@gmail.com>
2024-12-12 13:19:27 +0100

    mejora de la lógica de los querySelector

--- scrip.js
+++ scrip.js
@@ -1,18 +1,24 @@
-let keyboardId = document.querySelector(".numbers")
+let keyboardId = document.querySelectorAll("button")
 let display = document.querySelector("p")
 
 let n1 = []
-let n2 = []
-let result = []
+// let n2 = []
+// let result = []
 
-keyboardId.addEventListener("click",(e)=>{
-  n1.push(e.target.textContent)
-  let number = n1
-  display.textContent = number
-  concatenar()
-})
-function concatenar () {
-  for (let i = 0; i < n1.length; i++) {
-    console.log(n1[0]+n1[1]+n1[2]+n1[3])
-  }
+for (let i = 0; i < keyboardId.length; i++) {
+  keyboardId[i].addEventListener("click",(e)=>{
+    n1.push(e.target.textContent)
+    display.textContent = n1
+    console.log(n1)
+    console.log("hola")
+    // if (condition) {
+    // }
+    // concatenar()
+  })
 }
+// function concatenar () {
+//   for (let i = 0; i < n1.length; i++) {
+//     console.log(n1[0]+n1[1]+n1[2]+n1[3])
+//   }
+// }
+
