b6f281415e4e8f100e89affa28ef479300f73f37
Pablo Querales <pabloquerales20@gmail.com>
2024-12-22 13:38:32 +0100

    solución del problema del display

--- scrip.js
+++ scrip.js
@@ -1,18 +1,19 @@
-let keyboardId = document.querySelector(".numbers")
-let display = document.querySelector("p")
+let numberButtons = document.querySelectorAll(".number-buttons")
+let display = document.querySelector("spam")
 
 let n1 = []
 let n2 = []
 let result = []
 
-keyboardId.addEventListener("click",(e)=>{
-  n1.push(e.target.textContent)
-  let number = n1
-  display.textContent = number
-  concatenar()
-})
-function concatenar () {
-  for (let i = 0; i < n1.length; i++) {
-    console.log(n1[0]+n1[1]+n1[2]+n1[3])
-  }
+
+for (let i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener("click",(e)=>{
+    let number = ""  
+    n1.push(e.target.textContent)
+    for (let i = 0; i < n1.length; i++) {
+      number = number + n1[i]
+      display.textContent = number
+      }
+  })
 }
+
