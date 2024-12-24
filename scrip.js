15a80a26c48ce6d11f631c56b672a3430b47e991
Pablo Querales <pabloquerales20@gmail.com>
2024-12-24 13:45:15 +0100

    se creó la función de sumar

--- scrip.js
+++ scrip.js
@@ -1,12 +1,16 @@
 let numberButtons = document.querySelectorAll(".number-buttons")
-let operatorButtons = document.querySelectorAll(".operator-button")
+let additionButton = document.querySelector(".addition-button")
+let subtractionButton = document.querySelector("subtraction-button")
+let multiplicationButton = document.querySelector(".multiplication-button")
+let divisionButton = document.querySelector(".division-button")
+let equalButton = document.querySelector(".equal-button")
 let displayNumber = document.querySelector(".display-number")
 let displayOperator = document.querySelector(".display-operator")
+let displaySimbolOperator = document.querySelector(".display-simbol-operator")
+
 
 let n1 = []
 let n2 = []
-let result = n1 + n2
-
 
 for (let i = 0; i < numberButtons.length; i++) {
   numberButtons[i].addEventListener("click",(e)=>{
@@ -16,23 +20,37 @@ for (let i = 0; i < numberButtons.length; i++) {
       number = number + n1[i]
       displayNumber.textContent = number
     }
-    for (let i = 0; i < operatorButtons.length; i++){
-      operatorButtons[i].addEventListener("click", (e)=>{
-        if (e.target.textContent == "+") {
-          n1 = []          
-          number= 0
-          displayOperator.textContent = e.target.textContent
-        }
-        if(e.target.textContent == "-"){
-          displayOperator.textContent = e.target.textContent
-        }
-          if(e.target.textContent == "*"){
-          displayOperator.textContent = e.target.textContent
-        }
-          if(e.target.textContent == "%"){
-          displayOperator.textContent = e.target.textContent
-        }
-      })
-    }      
   })
 }
+// FUNCION DE SUMAR
+additionButton.addEventListener("click", (e)=>{
+  if(displayOperator.textContent == ""){
+    displaySimbolOperator.textContent = e.target.textContent
+    displayOperator.textContent = displayNumber.textContent
+    displayNumber.textContent = "0"
+    n1 = []
+    equalButton.addEventListener("click", ()=>{
+      displayNumber.textContent = Number(displayOperator.textContent)+Number(displayNumber.textContent)
+      displayOperator.textContent = ""
+      displaySimbolOperator.textContent = ""
+    })
+  } else {
+      displaySimbolOperator.textContent = e.target.textContent
+      displayOperator.textContent = Number(displayOperator.textContent)+Number(displayNumber.textContent)
+      displayNumber.textContent = "0"
+      n1 = []
+  }
+  })   
+
+
+    
+    // if(e.target.textContent == "-"){
+    //   displayOperator.textContent = e.target.textContent
+    // }
+    //   if(e.target.textContent == "*"){
+    //   displayOperator.textContent = e.target.textContent
+    // }
+    //   if(e.target.textContent == "%"){
+    //   displayOperator.textContent = e.target.textContent
+    // }
+
