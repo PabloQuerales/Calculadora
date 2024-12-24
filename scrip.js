3c29a22033968ba493ca8d9359e5ae8a507ec2af
Pablo Querales <pabloquerales20@gmail.com>
2024-12-24 14:05:18 +0100

    se agregó la función de resta aunque presenta problemas con la resta de numeros negativos

--- scrip.js
+++ scrip.js
@@ -1,13 +1,12 @@
 let numberButtons = document.querySelectorAll(".number-buttons")
 let additionButton = document.querySelector(".addition-button")
-let subtractionButton = document.querySelector("subtraction-button")
+let subtractionButton = document.querySelector(".subtraction-button")
 let multiplicationButton = document.querySelector(".multiplication-button")
 let divisionButton = document.querySelector(".division-button")
 let equalButton = document.querySelector(".equal-button")
 let displayNumber = document.querySelector(".display-number")
 let displayOperator = document.querySelector(".display-operator")
 let displaySimbolOperator = document.querySelector(".display-simbol-operator")
-
 
 let n1 = []
 let n2 = []
@@ -33,6 +32,7 @@ additionButton.addEventListener("click", (e)=>{
       displayNumber.textContent = Number(displayOperator.textContent)+Number(displayNumber.textContent)
       displayOperator.textContent = ""
       displaySimbolOperator.textContent = ""
+      n1 = []
     })
   } else {
       displaySimbolOperator.textContent = e.target.textContent
@@ -41,16 +41,23 @@ additionButton.addEventListener("click", (e)=>{
       n1 = []
   }
   })   
-
-
-    
-    // if(e.target.textContent == "-"){
-    //   displayOperator.textContent = e.target.textContent
-    // }
-    //   if(e.target.textContent == "*"){
-    //   displayOperator.textContent = e.target.textContent
-    // }
-    //   if(e.target.textContent == "%"){
-    //   displayOperator.textContent = e.target.textContent
-    // }
-
+// FUNCION DE RESTAR
+subtractionButton.addEventListener("click", (e)=>{
+  if(displayOperator.textContent == ""){
+    displaySimbolOperator.textContent = e.target.textContent
+    displayOperator.textContent = displayNumber.textContent
+    displayNumber.textContent = "0"
+    n1 = []
+    equalButton.addEventListener("click", ()=>{
+      displayNumber.textContent = Number(displayOperator.textContent)-Number(displayNumber.textContent)
+      displayOperator.textContent = ""
+      displaySimbolOperator.textContent = ""
+      n1 = []
+    })
+  } else {
+      displaySimbolOperator.textContent = e.target.textContent
+      displayOperator.textContent = Number(displayOperator.textContent)-Number(displayNumber.textContent)
+      displayNumber.textContent = "0"
+      n1 = []
+  }
+})
